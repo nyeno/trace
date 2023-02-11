a7758d80812a6c42c43b1e7101de36201a78405d
nyeno <enonyenooke@gmail.com>
2023-02-11 19:22:53 +0100

    test: testing operations on firebase db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app'
 
-import 'firebase/firestore'
+import {getFirestore} from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const config = {
@@ -16,6 +16,8 @@ const config = {
 const app = initializeApp(config)
 
 export const auth = getAuth(app)
+
+export const db = getFirestore(app)
 
 //export const firestore = firebase.firestore();
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import { React, useState, useEffect} from "react";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/UserAuthContext";
+import { db } from "../firebase/firebase.utils";
+import { collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const { logOut, user } = useUserAuth();
@@ -13,6 +15,20 @@ const Home = () => {
       console.log(error.message);
     }
   };
+  const [hackerNews, setHackerNews] = useState([])
+  const hackerNewsRef = collection(db, 'jobs', 'hackerNewsJobs', 'reactJobs')
+
+  useEffect(() => {
+    const getJobs = async() => {
+      const data = await getDocs(hackerNewsRef)
+      setHackerNews(data.docs.map((doc) => ({
+        ...doc.data(), id: doc.id
+      })))
+      console.log("My data:" , hackerNews)
+    }
+
+    getJobs()
+  }, [])
   return (
     <>
       <div className="p-4 box mt-3 text-center">
@@ -23,6 +39,22 @@ const Home = () => {
         <button variant="primary" onClick={handleLogout}>
           Log out
         </button>
+      </div>
+      <div>
+        {hackerNews.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>{user.companyName}</p>
+              <a
+                href={`mailto:${user.recruiterMail}`}
+              >
+                {" "}
+                Email {user.companyName}
+              </a>
+            </div>
+          );
+          
+        })}
       </div>
     </>
   );
